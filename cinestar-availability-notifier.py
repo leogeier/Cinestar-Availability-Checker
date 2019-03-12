1c2fd735ef076dcf36c158fa8b48dc8f28e70aae
leogeier <leonard.geier@hotmail.com>
2019-03-12 23:26:31 +0100

    add telegram bot

--- cinestar-availability-notifier.py
+++ cinestar-availability-notifier.py
@@ -1,6 +1,8 @@
 import requests
 import sys
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+from telegram.ext import Updater, CommandHandler
+from telegram import Bot
 
 NOT_AVAILABLE_INDICATOR = 'Dieser Film befindet sich aktuell nicht im Programm.'
 
@@ -8,6 +10,9 @@ def querySite(url):
     r = requests.get(url)
     if NOT_AVAILABLE_INDICATOR not in r.text:
         print("alarm")
+
+def ping_(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="pong")
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
@@ -20,6 +25,16 @@ if __name__ == '__main__':
     name = sys.argv[3]
 
     # Set up scheduler
-    scheduler = BlockingScheduler()
+    scheduler = BackgroundScheduler()
     scheduler.add_job(querySite, 'interval', seconds=interval, args=[url])
     scheduler.start()
+
+    # Set up bot
+    with open('.bot-token.txt') as f:
+        token = f.read()
+        updater = Updater(token=token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('ping', ping_))
+
+    updater.start_polling()
+    updater.idle()
