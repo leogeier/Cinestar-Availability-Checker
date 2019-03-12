3dfb18e4aea1ae843049aa968a660e3b3b72f529
leogeier <leonard.geier@hotmail.com>
2019-03-12 21:55:44 +0100

    scheduler

--- cinestar-availability-notifier.py
+++ cinestar-availability-notifier.py
@@ -1,10 +1,17 @@
 import requests
 import sys
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: [URL] [Interval in MS]")
+        print("Usage: [URL] [Interval in seconds]")
         sys.exit()
 
+    # Extract arguments
     url = sys.argv[1]
     interval = int(sys.argv[2])
+
+    # Set up scheduler
+    scheduler = BlockingScheduler()
+    scheduler.add_job(lambda: print(url), 'interval', seconds=interval)
+    scheduler.start()
